5a9c9173ed626a01f06bc7ab140e1eb272bab123
Pranjal Shukla <90145718+Pranjal826@users.noreply.github.com>
2023-04-03 14:20:13 +0530

    Update diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -4,7 +4,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn.metrics import accuracy_score
-diabetes_dataset = pd.read_csv('/content/diabetes.csv') 
+diabetes_dataset = pd.read_csv('diabetes.csv') 
 diabetes_dataset.shape
 diabetes_dataset.describe()
 diabetes_dataset['Outcome'].value_counts()
